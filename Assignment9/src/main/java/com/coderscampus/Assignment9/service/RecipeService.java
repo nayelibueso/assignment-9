25894a6fa3ffbc9d17fce870c32e98c36afc3494
nayelibueso <nayelibueso50@gmail.com>
2024-08-07 13:14:12 -0500

    Added methods to filter through all needed recipes for @GetMapping

--- Assignment9/src/main/java/com/coderscampus/Assignment9/service/RecipeService.java
+++ Assignment9/src/main/java/com/coderscampus/Assignment9/service/RecipeService.java
@@ -6,36 +6,68 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
 
 import com.coderscampus.Assignment9.domain.Recipe;
+
 //Will be loading and parsing the 'recipes.txt' using Apache commons CSVS
 //Storing parsed data into a collection
 //Providing methods to filter the recipes based on specific criteria
-@Service
+@Service // Service class that handles the buisnuess logic of this class
 public class RecipeService {
 	private List<Recipe> recipes = new ArrayList<>();
-	
-	public void loadRecipes () throws IOException {
+
+	public void loadRecipes() throws IOException {
 		Reader in = new FileReader("recipes.txt");
 		Iterable<CSVRecord> records = CSVFormat.DEFAULT.withIgnoreSurroundingSpaces().withHeader().parse(in);
 		for (CSVRecord record : records) {
-			String cookingMinutes = record.get(0);
-			Boolean dairyFree = Boolean.parseBoolean(record.get(1));
-			Boolean glutenFree = Boolean.parseBoolean(record.get(2));
-			String instructions = record.get(3);
-			Double preperationMinutes = Double.parseDouble(record.get(4));
-			Double pricePerServing = Double.parseDouble(record.get(5));
-			Integer readyInMinutes = Integer.parseInt(record.get(6));
-			Integer servings = Integer.parseInt(record.get(7));
-			Double spoonacularScore = Double.parseDouble(record.get(8));
-			String title = record.get(9);
-			Boolean vegan = Boolean.parseBoolean(record.get(10));
-			Boolean vegetarian = Boolean.parseBoolean(record.get(11)); 
-			
+			Recipe recipe = new Recipe();
+			recipe.setCookingMinutes(Integer.parseInt(record.get(0))); //Setting each fields for the 'Recipe' class
+			recipe.setDairyFree(Boolean.parseBoolean(record.get(1)));
+			recipe.setGlutenFree(Boolean.parseBoolean(record.get(2)));
+			recipe.setInstructions(record.get(3));
+			recipe.setPreparationMinutes(Double.parseDouble(record.get(4)));
+			recipe.setPricePerServing(Double.parseDouble(record.get(5)));
+			recipe.setReadyInMinutes(Integer.parseInt(record.get(6)));
+			recipe.setServings(Integer.parseInt(record.get(7)));
+			recipe.setSpoonacularScore(Double.parseDouble(record.get(8)));
+			recipe.setTitle(record.get(9));
+			recipe.setVegan(Boolean.parseBoolean(record.get(10)));
+			recipe.setVegetarian(Boolean.parseBoolean(record.get(11)));
+			recipes.add(recipe);
 		}
+	}
+
+	public List<Recipe> glutenFreeRecipes() {
+		return recipes.stream()
+				      .filter(Recipe::getGlutenFree)
+				      .collect(Collectors.toList());
+	}
+
+	public List<Recipe> veganRecipes() {
+		return recipes.stream()
+				      .filter(Recipe::getVegan)
+				      .collect(Collectors.toList());
+	}
+
+	public List<Recipe> veganAndGlutenFreeRecipes() {
+		return recipes.stream()
+				      .filter(Recipe::getGlutenFree)
+				      .filter(Recipe::getVegan)
+				      .collect(Collectors.toList());
+	}
+
+	public List<Recipe> vegetarianRecipes() {
+		return recipes.stream()
+				      .filter(Recipe::getVegetarian)
+				      .collect(Collectors.toList());
+	}
+
+	public List<Recipe> allRecipes() {
+		return recipes;
 	}
 }
--- Assignment9/src/main/java/com/coderscampus/Assignment9/web/RecipeController.java
+++ Assignment9/src/main/java/com/coderscampus/Assignment9/web/RecipeController.java
@@ -1,14 +1,29 @@
 package com.coderscampus.Assignment9.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.coderscampus.Assignment9.domain.Recipe;
+import com.coderscampus.Assignment9.service.RecipeService;
 
 //Acts as the interface between the user (front end) and the service layer (RecipeService)
 //Maps HTTP request (GET Requests) to specific methods 
 //Calls the 'RecipeService' to get the data and returns it as a response 
 @RestController
 public class RecipeController {
-
+	@Autowired
+	public RecipeService recipeService; //Instance of RecipeService class
+	
+	
 //	/gluten-free returns gluten-free recipes.
+	@GetMapping("/gluten-free") //Made to handle GET request
+	public List<Recipe> glutenFreeRecipes() {
+		recipeService.loadRecipes();
+		
+	}
 //	/vegan returns vegan recipes.
 //	/vegan-and-gluten-free returns recipes that are both vegan and gluten-free.
 //	/vegetarian returns vegetarian recipes.
